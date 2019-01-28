8360ce6069205d8279f764bb7bd28843f6e645b3
Andreas Linz <klingt.net@gmail.com>
2019-01-29 00:17:59 +0100

    Use new rand crate
    The crate is now splitted, thus rand becomes rand_core and rand_xorshift
    in my case. Because of safe number conversion f64's are used in the
    benchmark.

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,10 +1,12 @@
 #![feature(test)]
 
-extern crate rand;
+extern crate rand_core;
+extern crate rand_xorshift;
 extern crate rb;
 extern crate test;
 
-use rand::{Rng, XorShiftRng};
+use rand_core::{RngCore, SeedableRng};
+use rand_xorshift::XorShiftRng;
 use rb::{RbConsumer, RbProducer, SpscRb, RB};
 use std::thread;
 use test::Bencher;
@@ -16,16 +18,24 @@ fn bench_passing_a_1k_buffer_blocking(b: &mut Bencher) {
     let rb = SpscRb::new(SIZE);
     let producer = rb.producer();
     let consumer = rb.consumer();
-    let mut rng = XorShiftRng::new_unseeded();
+    let mut rng = XorShiftRng::from_seed([0u8; 16]);
     let data = (0..SIZE)
-        .map(|_| rng.gen_range(-1.0f32, 1.0f32))
-        .collect::<Vec<f32>>();
+        .map(|_| rand_float(&mut rng))
+        .collect::<Vec<f64>>();
     thread::spawn(move || loop {
         producer.write_blocking(&data).unwrap();
     });
-    let mut buf = [0f32; SIZE];
+    let mut buf = [0f64; SIZE];
     b.iter(|| {
         let cnt = consumer.read_blocking(&mut buf).unwrap();
         assert_eq!(cnt, SIZE);
     });
+}
+
+fn rand_float(rng: &mut XorShiftRng) -> f64 {
+    let r = rng.next_u32();
+    if r == 0 {
+        return 0.0;
+    }
+    f64::from(u32::max_value() / (r - i32::max_value() as u32))
 }
